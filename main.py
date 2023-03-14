24503943119caa90643af1c1c8cb51487f26d228
Alejandro Calderón <robertoalejandrocalderon@gmail.com>
2023-03-14 09:32:57 -0600

    Inciso 2
    Se evaluó si el numero era par o impar y se imprimio su residuo.

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 mayor = 0
 residuo = 0
+
+residuo = mayor % 2
+if residuo == 0:
+    print(f'El número {mayor} es par porque su residuo es 0.')
+else:
+    print(f'El número {mayor} es impar porque su residuo es {residuo}.')
