5f05d05ef08278c9b5a5e3b2d7bc559f7f21a4e3
IvanOrdonez22 <ivanalex2205@gmail.com>
2023-03-14 09:30:34 -0600

    Inciso1
    Se pidió al usuario 2 numeros enteros y se encontró el numero mayor de los 2

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 mayor = 0
 residuo = 0
+num1 = int(input("Ingrese su primer numero: "))
+num2 =  int(input("Ingrese su segundo numero: "))
+if num1 > num2:
+        mayor = num1
+        print(mayor)
+elif num2 > num1:
+        mayor = num2
+        print(mayor)
+else:
+    print(f'{num1} y {num2} son iguales')
+    mayor = num1
